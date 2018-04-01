632e614664feef2894994e49fb5a04d870459c14
mploed <michael.ploed@gmail.com>
2018-04-01 16:12:19 +0200

    Spring Boot Application for Credit Sales Funnel

---
+++ credit-sales-funnel/src/main/java/com/mploed/dddwithspring/creditsalesfunnel/CreditSalesFunnelApplication.java
@@ -0,0 +1,12 @@
+package com.mploed.dddwithspring.creditsalesfunnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSalesFunnelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditSalesFunnelApplication.class, args);
+	}
+}
