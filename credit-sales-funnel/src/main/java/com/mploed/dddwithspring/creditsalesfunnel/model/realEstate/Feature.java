8b6a9b670cb0f6a72612adc234489a1a646d1a42
mploed <michael.ploed@gmail.com>
2018-04-14 11:49:33 +0200

    UI Improvements

--- credit-sales-funnel/src/main/java/com/mploed/dddwithspring/creditsalesfunnel/model/realEstate/Feature.java
+++ credit-sales-funnel/src/main/java/com/mploed/dddwithspring/creditsalesfunnel/model/realEstate/Feature.java
@@ -1,5 +1,23 @@
 package com.mploed.dddwithspring.creditsalesfunnel.model.realEstate;
 
 public enum Feature {
-	SOLAR_COLLECTORS, PHOTOVOLTAICS, SPA, TWO_OR_MORE_BATHS, EV_CHARGER, POOL, SUMMERHOUSE_IN_GARDEN, SAT_TV
+	SOLAR_COLLECTORS("Solar collectors"),
+	PHOTOVOLTAICS("Photovoltaics"),
+	SPA("Spa"),
+	TWO_OR_MORE_BATHS("Two or more baths"),
+	EV_CHARGER("Charger for electric vehicle"),
+	POOL("Pool"),
+	SUMMERHOUSE_IN_GARDEN("Summerhouse in the garden"),
+	SAT_TV("Satellite TV");
+
+	private final String displayName;
+
+	Feature(String displayName) {
+		this.displayName = displayName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
 }
